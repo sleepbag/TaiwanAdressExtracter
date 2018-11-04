d16d8f7cb7cb179350a48cf8bc9d98e877d7dcbe
DESKTOP-EU0IGBT\sleepbag <x870381@gmail.com>
2018-11-04 22:17:25 +0800

    更改測試註記

--- tests.js
+++ tests.js
@@ -1,4 +1,4 @@
-QUnit.test( "origin test", function( assert ) {
+QUnit.test( "原始測試", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市鶯歌區中山路111巷1弄1號");
   var result = adress.Process();
@@ -6,7 +6,7 @@ QUnit.test( "origin test", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號", "Passed!" );
 });
 
-QUnit.test( "add name after test", function( assert ) {
+QUnit.test( "增加無關字串在後，例如姓名", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市鶯歌區中山路111巷1弄1號陳小寶");
   var result = adress.Process();
@@ -14,7 +14,7 @@ QUnit.test( "add name after test", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號", "Passed!" );
 });
 
-QUnit.test( "add name before test", function( assert ) {
+QUnit.test( "增加無關字串在前，例如姓名", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("陳小寶新北市鶯歌區中山路111巷1弄1號");
   var result = adress.Process();
@@ -22,7 +22,7 @@ QUnit.test( "add name before test", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號", "Passed!" );
 });
 
-QUnit.test( "add number test", function( assert ) {
+QUnit.test( "地址最後含有中文數字", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市鶯歌區中山路111巷1弄1號2樓之一");
   var result = adress.Process();
@@ -30,7 +30,7 @@ QUnit.test( "add number test", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號2樓之一", "Passed!" );
 });
 
-QUnit.test( "add arabian number test", function( assert ) {
+QUnit.test( "地址最後含有阿拉伯數字", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市鶯歌區中山路111巷1弄1號2樓之1");
   var result = adress.Process();
@@ -38,7 +38,7 @@ QUnit.test( "add arabian number test", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號2樓之1", "Passed!" );
 });
 
-QUnit.test( "mix number test 1", function( assert ) {
+QUnit.test( "地址最後使用符號 - 及阿拉伯數字", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市鶯歌區中山路111巷1弄1號2樓-1");
   var result = adress.Process();
@@ -46,7 +46,7 @@ QUnit.test( "mix number test 1", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號2樓-1", "Passed!" );
 });
 
-QUnit.test( "mix number test 2", function( assert ) {
+QUnit.test( "地址最後使用符號 - 及中文數字", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市鶯歌區中山路111巷1弄1號2樓-三");
   var result = adress.Process();
@@ -54,7 +54,7 @@ QUnit.test( "mix number test 2", function( assert ) {
   assert.ok( result == "新北市鶯歌區中山路111巷1弄1號2樓-三", "Passed!" );
 });
 
-QUnit.test( "miss area test", function( assert ) {
+QUnit.test( "即使原始地址缺少「區」、「鄉」，是否正常擷取", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("台北市中山北路二段1巷2號9樓");
   var result = adress.Process();
@@ -62,7 +62,7 @@ QUnit.test( "miss area test", function( assert ) {
   assert.ok( result == "台北市中山北路二段1巷2號9樓", "Passed!" );
 });
 
-QUnit.test( "normal test 1", function( assert ) {
+QUnit.test( "地址中一般的數字測試", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("新北市板橋區忠孝路127號");
   var result = adress.Process();
@@ -70,7 +70,7 @@ QUnit.test( "normal test 1", function( assert ) {
   assert.ok( result == "新北市板橋區忠孝路127號", "Passed!" );
 });
 
-QUnit.test( "normal test 2", function( assert ) {
+QUnit.test( "複雜字串綜合測試", function( assert ) {
 
   var adress = new TaiwanAdressExtracter("收貨地址：陳小寶，0911111111，台灣 新北市 汐止區新台五路一段268號2樓");
   var result = adress.Process();
@@ -78,56 +78,49 @@ QUnit.test( "normal test 2", function( assert ) {
   assert.ok( result == "新北市汐止區新台五路一段268號2樓", "Passed!" );
 });
 
-QUnit.test( "normal test 3", function( assert ) {
+QUnit.test( "綜合測試", function( assert ) {
   var adress = new TaiwanAdressExtracter("台北市中山北路二段1巷2號9樓2");
   var result = adress.Process();
   //console.log(result);
   assert.ok( result == "台北市中山北路二段1巷2號9樓2", "Passed!" );
 });
 
-QUnit.test( "city test 1", function( assert ) {
+QUnit.test( "加入無關字串綜合測試", function( assert ) {
   var adress = new TaiwanAdressExtracter("商店街市集大拍賣 台北市中山北路二段1巷2號9樓2");
   var result = adress.Process();
   //console.log(result);
   assert.ok( result == "台北市中山北路二段1巷2號9樓2", "Passed!" );
 });
 
-QUnit.test( "city test 2", function( assert ) {
+QUnit.test( "地址中複雜數字", function( assert ) {
   var adress = new TaiwanAdressExtracter("台北市中山北路二段115巷20號9樓之1");
   var result = adress.Process();
 //  console.log(result);
   assert.ok( result == "台北市中山北路二段115巷20號9樓之1", "Passed!" );
 });
 
-QUnit.test( "city test 3", function( assert ) {
+QUnit.test( "地址中複雜數字加符號", function( assert ) {
   var adress = new TaiwanAdressExtracter("台北市杭州南路一段15-1號11樓");
   var result = adress.Process();
   //console.log(result);
   assert.ok( result == "台北市杭州南路一段15-1號11樓", "Passed!" );
 });
 
-QUnit.test( "city test 4", function( assert ) {
-  var adress = new TaiwanAdressExtracter("收貨地址：程佳雯，0937580371，台灣 新北市 汐止區新台五路一段268號2樓");
-  var result = adress.Process();
+QUnit.test( "含有不是地址的複雜資訊", function( assert ) {
+  var adress = new TaiwanAdressExtracter("收貨地址：程O雯，0900000000，台灣 新北市 汐止區新台五路一段268號2樓");
+  var result = adress.Process();                           
   //console.log(result);
   assert.ok( result == "新北市汐止區新台五路一段268號2樓", "Passed!" );
 });
 
-QUnit.test( "city test 5", function( assert ) {
+QUnit.test( "綜合測試-新北", function( assert ) {
   var adress = new TaiwanAdressExtracter("新北市板橋區忠孝路127號");
   var result = adress.Process();
   //console.log(result);
   assert.ok( result == "新北市板橋區忠孝路127號", "Passed!" );
 });
 
-QUnit.test( "city test 6", function( assert ) {
-  var adress = new TaiwanAdressExtracter("台北縣土城市中央路四段281-13號三樓");
-  var result = adress.Process();
-  console.log(result);
-  assert.ok( result == "新北市土城市中央路四段281-13號三樓", "Passed!" );
-});
-
-QUnit.test( "city test 7", function( assert ) {
+QUnit.test( "開頭不完整地址排除", function( assert ) {
   var adress = new TaiwanAdressExtracter("台北市北投區台北市東華街一段502號11F");
   var result = adress.Process();
   console.log(result);
@@ -135,16 +128,23 @@ QUnit.test( "city test 7", function( assert ) {
 });
 
 
-QUnit.test( "city test 8", function( assert ) {
+QUnit.test( "在地址後增加了地標註記，並且前面有郵遞區後等無關字串", function( assert ) {
   var adress = new TaiwanAdressExtracter("333桃園市龜山區文化一路259號長庚大學第一醫學大樓七樓寄生蟲科");
   var result = adress.Process();
   console.log(result);
   assert.ok( result == "桃園市龜山區文化一路259號長庚大學第一醫學大樓七樓寄生蟲科", "Passed!" );
 });
 
-QUnit.test( "city test 9", function( assert ) {
+QUnit.test( "桃園縣轉桃園市", function( assert ) {
   var adress = new TaiwanAdressExtracter("桃園縣蘆竹鄉吉林路103號9樓");
   var result = adress.Process();
   console.log(result);
   assert.ok( result == "桃園市蘆竹鄉吉林路103號9樓", "Passed!" );
+});
+
+QUnit.test( "台北縣轉換新北市", function( assert ) {
+  var adress = new TaiwanAdressExtracter("台北縣土城市中央路四段281-13號");
+  var result = adress.Process();
+  console.log(result);
+  assert.ok( result == "新北市土城市中央路四段281-13號", "Passed!" );
 });
